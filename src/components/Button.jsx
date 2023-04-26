f12e194d99673018a940d56a6b8c573775c6e861
Mae <funj451@gmail.com>
2023-04-26 20:12:53 +0700

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+function Button({ label, onClick }) {
+  return <button>{label}</button>;
+}
+
+export default Button;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,5 @@
+function CommentBox({ label, onClick }) {
+  return <button>{label}</button>;
+}
+
+export default CommentBox;
