bfbe55b47a7421800b79ae761f90b975ed71c508
Mae <funj451@gmail.com>
2023-04-29 10:38:45 +0700

    update jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -43,3 +43,30 @@ function App() {
 }
 
 export default App;
+
+// import "./App.css";
+// import { useState, useEffect } from "react";
+
+// function App() {
+//   const API = "https://api.quotable.io/quotes/random";
+//   const [quote, setQuote] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchQuote() {
+//       const response = await fetch(API);
+//       const data = await response.json();
+
+//       setQuote(data);
+//     }
+
+//     fetchQuote();
+//   }, []);
+
+//   if (quote === null) {
+//     return <p>Loading random quote data...</p>;
+//   }
+
+//   return <div>{quote[0].content}</div>;
+// }
+
+// export default App;
