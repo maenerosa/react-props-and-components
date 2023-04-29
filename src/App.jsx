e5e284a2094292073a8d3bf0aff5fed981ea280c
Mae <funj451@gmail.com>
2023-04-29 10:17:11 +0700

    update jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
+
+    if (inputValue === "") return;
+
     setTodos([...todos, inputValue]);
     setInputValue("");
   };
