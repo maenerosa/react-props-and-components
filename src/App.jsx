6467660de714955d4fbbb049f07f499b474d943b
Mae <funj451@gmail.com>
2023-04-29 09:37:12 +0700

    updated json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import "./App.css";
-import List from "./components/List";
+import { useState } from "react";
 
 function App() {
-  const todos = [
-    "Learn React",
-    "Learn Redux",
-    "Learn GraphQL",
-    "Learn MongoDB",
-    "Learn NodeJS",
-    "Learn Express",
-    "Learn AWS",
-  ];
+  const [todos, setTodos] = useState(["Wake up!", "Code", "Sleep"]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  const handleInputChange = (event) => setInputValue(event.target.value);
+
+  const handleTodoDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+
+    setTodos(newTodos);
+  };
 
   return (
-    <div className="container">
-      <List items={todos} />
+    <div>
+      <h1>Todo App</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleTodoDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
